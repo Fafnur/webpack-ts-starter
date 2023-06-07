72175f86e741f0f7abe3836e5ebd4138c03693be
Aleksandr Serenko <fafnur@yandex.ru>
2023-06-07 16:43:35 +0700

    [vendors] Updating vendors

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = ({ file, options, env }) => ({
   parser: false,
   plugins: {
     'postcss-import': {},
-    'postcss-cssnext': {},
+    'autoprefixer': {},
     cssnano: env === 'production' ? {} : false,
   },
 });
--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -1,4 +1,4 @@
-import { Configuration } from "webpack";
+import { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 
 import common from './webpack.config';
